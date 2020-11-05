6f4ef8e2eab42d7620eafa3b103724e70c6b98d9
Christopher Wong <cwong51799@gmail.com>
2020-11-05 14:08:01 -0500

    Sheep will now startle other sheep
    Sheep will now startle other nearby sheep for 2 seconds. Also adjusted Sheep FOV and added some documentation on the sheep.

--- Assets/Scripts/SheepMovementScript.cs
+++ Assets/Scripts/SheepMovementScript.cs
@@ -3,26 +3,43 @@ using System.Collections.Generic;
 using UnityEngine.AI;
 using UnityEngine;
 
+
+
+/* The sheep has a few states:
+1. Grazing: When the sheep does not detect any nearby wolf. A sheep is grazing when there is no wolf detected.
+            A sheep will pick a new location on the NavMesh every 10 seconds to path to.
+2. Running: When the sheep has detected a wolf through hearing/sight and is broadcasting a startle.
+            The difference between running and startled is subtle. A running sheep will not be able to be
+            startled, meaning a sheep will focus on running away from the wolf it sees before entering a state of startle.
+            A sheep is deterministically in a running state when there is a wolf detected but it is not startled.
+3. Startled: When the sheep was alerted by a nearby sheep to run away. A sheep is in the startled state when there is a wolf detected an it is startled.
+
+*/
 public class SheepMovementScript : MonoBehaviour
 {
     public Camera vision;
 
     public GameObject wolf1, wolf2, wolf3;
 
+    GameObject[] wolves;
+
     public NavMeshAgent agent;
-
-    public float audibleDistance = 10f;
     public float runSpeed = 10f;
-
+    public float detectionRadius = 10f;
     public float grazingSpeed = 5f;
 
     public float newPathFrequency = 10f;
-    float time = 0.0f;
 
 
+    // Startling
+    float startleTimer = 0f;
 
+    public float startleRadius = 15f;
 
-    GameObject[] wolves;
+    bool receivingStartle = false;
+
+    GameObject wolfDetected;
+
 
     // Found here https://answers.unity.com/questions/8003/how-can-i-know-if-a-gameobject-is-seen-by-a-partic.html
     // Checks if the wolf is visible.
@@ -39,7 +56,7 @@ public class SheepMovementScript : MonoBehaviour
 
     bool isWolfAudible(GameObject wolf) {
         float dist = Vector3.Distance(this.transform.position, wolf.transform.position);
-        if(dist < audibleDistance) {
+        if(dist < detectionRadius) {
             return true;
         }
         return false;
@@ -76,46 +93,66 @@ public class SheepMovementScript : MonoBehaviour
     }
 
 
-    void moveForward() {
-        this.transform.position += Vector3.forward * Time.deltaTime * grazingSpeed;
+    // When a sheep detects a wolf, create a chain reaction startling all other sheep within a certain radius to run away from the wolf.
+    // This forces the player to keep an eye out for nearby sheep alerting eachother.
+    // The startle radius should somewhat scale with the sheep's FOV/view distance I think. Need to find a good number for that.
+    void startleNearbySheep(GameObject wolf) {
+        Collider[] hitColliders = Physics.OverlapSphere(this.gameObject.transform.position, startleRadius);
+        foreach (var hitCollider in hitColliders)
+        {
+            // If it has a sheep movement script, it's a sheep. Otherwise the get component will return null and the rest will not execute.
+            SheepMovementScript sheepScript = hitCollider.gameObject.GetComponent<SheepMovementScript>();
+            // Startle all nearby sheep, but not yourself (would lead to infinite loop)
+            if(sheepScript && hitCollider.gameObject != this.gameObject) {
+                sheepScript.startle(wolfDetected);
+            }
+        }
     }
 
-    void moveBackward() {
-        this.transform.position += -Vector3.forward * Time.deltaTime * grazingSpeed;
+
+    // Sets the sheep's wolf detected to the original wolf for 2 seconds
+    // Only startle a sheep if it wasn't currently running away from a wolf.
+    void startle(GameObject wolf) {
+        if (wolfDetected == null) {
+            setWolfDetected(wolf);
+            startleTimer = 2;
+            receivingStartle = true;
+        }
     }
 
-    void moveLeft() {
-        this.transform.position += Vector3.left * Time.deltaTime * grazingSpeed;
+    // Check the startle timer, decrease it once per second. Once it times out then set receiving startle to false
+    // so that the sheep can start detecting nearby wolves again.
+    void checkForUnstartiling() {
+        if (startleTimer > 0) {
+            startleTimer -= Time.deltaTime;
+        } else {
+            // No longer detect the wolf
+            startleTimer = 0;
+            wolfDetected = null;
+            receivingStartle = false;
+        }
     }
 
-    void moveRight() {
-        this.transform.position += Vector3.right * Time.deltaTime * grazingSpeed;
-    }
-    
-    void graze() {
-        //moveForward();
-        moveBackward();
-        //moveLeft();
-        moveRight();
-
+    // Temporarily set their wolf detected.
+    void setWolfDetected(GameObject wolf) {
+        wolfDetected = wolf;
     }
 
-    void turnOccasionally() {
-        transform.Rotate(0,90,0);
-    }
-
+    // Randomly pick a location on the NavMesh and set it as a location. Only do this if there is currently no wolf detected.
     void findANewDestination() {
-        // Distance the random target should be
-        float walkRadius = 100;
-        // Pick a random direction
-        Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
-        randomDirection += transform.position;
-        // Pick a random position
-        NavMeshHit hit;
-        NavMesh.SamplePosition(randomDirection, out hit, walkRadius, 1);
-        Vector3 finalPosition = hit.position;
-        // Send the sheep there.
-        agent.SetDestination(finalPosition);
+        if (!wolfDetected) {
+            // Distance the random target should be
+            float walkRadius = 200;
+            // Pick a random direction
+            Vector3 randomDirection = Random.insideUnitSphere * walkRadius;
+            randomDirection += transform.position;
+            // Pick a random position
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomDirection, out hit, walkRadius, 1);
+            Vector3 finalPosition = hit.position;
+            // Send the sheep there.
+            agent.SetDestination(finalPosition);
+        }
     }
 
 
@@ -129,17 +166,22 @@ public class SheepMovementScript : MonoBehaviour
     {
         wolves = new GameObject[] {wolf1, wolf2, wolf3};
         // Occasionally find a new destination and start walking there.
-        InvokeRepeating("findANewDestination",0,newPathFrequency);
+        InvokeRepeating("findANewDestination", 0, newPathFrequency);
     }
 
     // Update is called once per frame
     void Update()
     { 
-        GameObject wolfInSight = searchForWolves();
-        // Run in the opposite direction
-        if (wolfInSight != null) {
-            runAway(wolfInSight);
+        // Run in panic if another sheep told you to.
+        if (!receivingStartle) {
+            wolfDetected = searchForWolves();
         }
+        // Run in the opposite direction
+        if (wolfDetected != null) {
+            runAway(wolfDetected);
+            startleNearbySheep(wolfDetected);
+        }
+        checkForUnstartiling();
     }
 
 }
